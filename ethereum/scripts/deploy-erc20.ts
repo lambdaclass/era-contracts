f97c03ac20b9c5b7246cedb544fa3fa4f85460b4
juanbono <juanbono94@gmail.com>
2023-12-22 17:31:50 -0300

    remove console.error

--- ethereum/scripts/deploy-erc20.ts
+++ ethereum/scripts/deploy-erc20.ts
@@ -58,10 +58,6 @@ async function deployToken(token: TokenDescription, wallet: Wallet): Promise<Tok
 async function approve(token: TokenDescription, wallet: Wallet, spenderAddress: String): Promise<void> {
   token.implementation = token.implementation || DEFAULT_ERC20;
   const erc20 = (await hardhat.ethers.getContractFactory(token.implementation, wallet)).attach(token.address);
-  console.error("WALLET ADDRESS");
-  console.error(wallet.address);
-  console.error("WALLET PRIVATE KEY");
-  console.error(wallet._signingKey().privateKey);
   await erc20.mint(wallet.address, parseEther("300000000000000000000000000"));
   await erc20.approve(spenderAddress, parseEther("300000000000000000000000000"));
 
@@ -93,11 +89,6 @@ async function main() {
         ? new Wallet(cmd.privateKey, provider)
         : Wallet.fromMnemonic(ethTestConfig.mnemonic, "m/44'/60'/0'/0/1").connect(provider);
 
-      console.error("DEPLOYER ADDRESS");
-      console.error(wallet.address);
-      console.error("DEPLOYER PRIVATE KEY");
-      console.error(wallet._signingKey().privateKey);
-
       console.log(JSON.stringify(await deployToken(token, wallet), null, 2));
     });
 
@@ -112,11 +103,6 @@ async function main() {
       const wallet = cmd.privateKey
         ? new Wallet(cmd.privateKey, provider)
         : Wallet.fromMnemonic(ethTestConfig.mnemonic, "m/44'/60'/0'/0/1").connect(provider);
-
-      console.error("DEPLOYER ADDRESS");
-      console.error(wallet.address);
-      console.error("DEPLOYER PRIVATE KEY");
-      console.error(wallet._signingKey().privateKey);
 
       for (const token of tokens) {
         result.push(await deployToken(token, wallet));
@@ -142,19 +128,9 @@ async function main() {
         ? new Wallet(cmd.privateKey, provider)
         : Wallet.fromMnemonic(ethTestConfig.mnemonic, "m/44'/60'/0'/0/1").connect(provider);
 
-      console.error("DEPLOYER ADDRESS");
-      console.error(wallet.address);
-      console.error("DEPLOYER PRIVATE KEY");
-      console.error(wallet._signingKey().privateKey);
-
       console.log(JSON.stringify(await approve(token, wallet, cmd.spenderAddress), null, 2));
 
       wallet = Wallet.fromMnemonic(ethTestConfig.mnemonic, "m/44'/60'/0'/0/0").connect(provider);
-
-      console.error("DEPLOYER ADDRESS");
-      console.error(wallet.address);
-      console.error("DEPLOYER PRIVATE KEY");
-      console.error(wallet._signingKey().privateKey);
 
       console.log(JSON.stringify(await approve(token, wallet, cmd.spenderAddress), null, 2));
     });
