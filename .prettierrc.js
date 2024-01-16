86bbbf3e95ea9390ab92641b1b11c2ad2dbc7e96
Ivan Litteri <ivanlitteri@Ivans-MacBook-Pro.local>
2024-01-16 10:09:35 -0300

    Revert zk fmt

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    ...require('@matterlabs/prettier-config'),
-    plugins: ['prettier-plugin-solidity'],
-    overrides: [
-        {
-            files: '*.sol',
-            options: {
-                bracketSpacing: false,
-                printWidth: 120,
-                singleQuote: false,
-                tabWidth: 4,
-                useTabs: false
-            }
-        }
-    ]
+  ...require("@matterlabs/prettier-config"),
+  plugins: ["prettier-plugin-solidity"],
+  overrides: [
+    {
+      files: "*.sol",
+      options: {
+        bracketSpacing: false,
+        printWidth: 120,
+        singleQuote: false,
+        tabWidth: 4,
+        useTabs: false,
+      },
+    },
+  ],
 };
